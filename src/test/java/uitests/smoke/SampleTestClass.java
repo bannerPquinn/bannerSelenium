f9eb6a9ddea4e9ed5dacb576d06dadaa603b3511
bannerPquinn <Patrick.Quinn@ellucian.com>
2016-04-20 11:00:51 -0400

    Finished method to print from the dataprovider and use TestCaseDetails object.

--- src/test/java/uitests/smoke/SampleTestClass.java
+++ src/test/java/uitests/smoke/SampleTestClass.java
@@ -1,5 +1,9 @@
 package uitests.smoke;
 
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Map;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
@@ -11,39 +15,35 @@ import org.testng.annotations.Test;
 
 import utils.BaseTest;
 import utils.SqlHandler;
+import utils.TestCaseDetails;
 
-public class SampleTestClass extends BaseTest
+public class SampleTestClass  extends BaseTest 
 {
     private WebElement element;
 
     @Parameters({ "expected_text" })
-    @Test(groups = { "Smoke", "SampleModule" })
-    public void testMethod(String expected_text)
+    @Test(groups = { "Regression"})
+    //Selenium test with example testng parameters
+    public void testMethod(String expected_text) throws InterruptedException
     {
-    	
-    	
-    	SqlHandler.sqlTransform();
-		
-		
         element = new WebDriverWait(driver, 25).until(ExpectedConditions.visibilityOfElementLocated(By.name("q")));
-        try {
-            Thread.sleep(5000);
-        } catch(InterruptedException ex) {
-            Thread.currentThread().interrupt();
-        }
+        Thread.sleep(5000);
         element.sendKeys("Hello There!" + Keys.RETURN);
-        try {
-            Thread.sleep(5000);
-        } catch(InterruptedException ex) {
-            Thread.currentThread().interrupt();
-        }
-        /*element = driver.findElement(By.name("btnK"));
-        element.click();
-        try {
-            Thread.sleep(5000);
-        } catch(InterruptedException ex) {
-            Thread.currentThread().interrupt();
-        }*/
+        Thread.sleep(5000);
         Assert.assertTrue(driver.getPageSource().contains(expected_text), "Expected text not found on page!");
+    }  
+    @Test(groups = { "Smoke" }, dataProvider = "sqlHandler", dataProviderClass = SqlHandler.class)
+    //first test of using custom sql dataprovider
+    public void testSqlMethod(TestCaseDetails a)
+    {
+    	//System.out.println("ID " + id + " first name = " + FirstName + " Last Name " + LastName);
+    	//System.out.println("Hello World");		
+    		Map rowDetails = a.detail;
+    		
+    		System.out.println("ID is " + rowDetails.get("ID") +
+    						   " First Name is " + rowDetails.get("FirstName") +
+    						   " Last Name is " + rowDetails.get("LastName"));
+    		
+    	
     }
 }
