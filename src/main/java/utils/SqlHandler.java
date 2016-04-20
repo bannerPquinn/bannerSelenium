da110ae7bc62aa8cdbcf52e7730d14a513bf22f7
bannerPquinn <Patrick.Quinn@ellucian.com>
2016-04-20 11:01:43 -0400

    Changed the return value to object [][] and changed it to return rows of testcase details to make it easier to use when writing a new test.

--- src/main/java/utils/SqlHandler.java
+++ src/main/java/utils/SqlHandler.java
@@ -1,50 +1,118 @@
 package utils;
 
+import java.lang.reflect.Method;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 
+import org.testng.ITestContext;
 import org.testng.annotations.DataProvider;
 
 
 public class SqlHandler {
 	
 	
-	//@DataProvider(name = "sqlHandler")
-	public static void sqlTransform() 
-	{
-		Connection con;
-		Statement stmt;
-		ResultSet rs;
-		
-		String url1 = "jdbc:mysql://localhost:3306/seleniumautomation";
-        String user = "pquinn";
-        String password = "u_pick_it";
-		
-		try {
-			
-			try {
-				Class.forName("com.mysql.jdbc.Driver").newInstance();
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
+	@DataProvider(name = "sqlHandler")
+	public static Object[][] sqlTransform(Method m) 
+	{		
+			String testCase = m.getName();
+			Connection connection = null;
+		    PreparedStatement statement = null;
+		    ResultSet resultSet = null;
+		    
+		    String url1 = "jdbc:mysql://localhost:3306/seleniumautomation";
+	        String user = "pquinn";
+	        String password = "u_pick_it";
+	        
+
+		    try {
+		    	
+		    	Class.forName("com.mysql.jdbc.Driver").newInstance();
+		        connection = DriverManager.getConnection(url1, user, password);
+		        statement = connection.prepareStatement("SELECT * FROM " + testCase);
+		        resultSet = statement.executeQuery();
+		        ResultSetMetaData rsmd = resultSet.getMetaData();
+		        
+		        int numcols = rsmd.getColumnCount();
+		        Object [][] result = new TestCaseDetails[getRowCount(resultSet)][1];
+		        
+		        int counter = 0;
+		        while (resultSet.next()) 
+		        {
+		            TestCaseDetails row = new TestCaseDetails(); // new list per row
+		            for (int i=1; i<= numcols; i++) 
+		            {  // don't skip the last column, use <=
+		                row.detail.put(rsmd.getColumnName(i), resultSet.getString(i));
+		                System.out.print(resultSet.getString(i) + "\t");
+		            }
+		            result[counter][0] = row; // add it to the result
+		            System.out.print("\n");
+		            counter ++;
+		        }
+		        
+		        return result;
+		    } 
+		    catch (Exception e) 
+			{
 				e.printStackTrace();
 			}
-		    con = DriverManager.getConnection(url1, user, password);
-		    stmt = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_READ_ONLY);
-			rs = stmt.executeQuery("SELECT * FROM testtable");
+		    finally {
+		        if (resultSet != null) try { resultSet.close(); } catch (SQLException logOrIgnore) {}
+		        if (statement != null) try { statement.close(); } catch (SQLException logOrIgnore) {}
+		        if (connection != null) try { connection.close(); } catch (SQLException logOrIgnore) {}
+		    }
+
+		    return null;
 			
-			while(rs.next())
-			{
-				System.out.println(rs.getInt("ID") + " " + rs.getString("FirstName") + " " + rs.getString("LastName"));
-			}
-		    
-		} catch (SQLException ex) {
-		    // handle any errors
-		    System.out.println("SQLException: " + ex.getMessage());
-		    System.out.println("SQLState: " + ex.getSQLState());
-		    System.out.println("VendorError: " + ex.getErrorCode());
+		 
+	}
+	public static void closeResultSet(ResultSet rs) 
+	{
+	    if (rs != null) {
+	        try 
+	        {
+	            rs.close();
+	        } 
+	        catch (SQLException ex) 
+	        {
+	            System.out.println("SQL Exception");
+	        }
+	    }
+	}
+	public List<HashMap<String,Object>> convertResultSetToList(ResultSet rs) throws SQLException {
+	    ResultSetMetaData md = rs.getMetaData();
+	    int columns = md.getColumnCount();
+	    List<HashMap<String,Object>> list = new ArrayList<HashMap<String,Object>>();
+
+	    while (rs.next()) {
+	        HashMap<String,Object> row = new HashMap<String, Object>(columns);
+	        for(int i=1; i<=columns; ++i) {
+	            row.put(md.getColumnName(i),rs.getObject(i));
+	        }
+	        list.add(row);
+	    }
+
+	    return list;
+	}
+	public static int getRowCount(ResultSet result)
+	{
+		int size = 0;
+		try {
+		    result.last();
+		    size = result.getRow();
+		    result.beforeFirst();
 		}
-		//return Object [][];
+		catch(Exception ex) {
+		    return 0;
+		}
+		return size;
 	}
 	
 	
 	
 }
+
